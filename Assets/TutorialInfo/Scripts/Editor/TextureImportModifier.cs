caf5a170f214a4e54730a05f730045866ea847c0
gjh <kevinbina@163.com>
2023-11-14 21:01:01 +0800

    解决报错

--- Assets/TutorialInfo/Scripts/Editor/TextureImportModifier.cs
+++ Assets/TutorialInfo/Scripts/Editor/TextureImportModifier.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 
-namespace Editor {
+namespace TutorialInfo.Scripts.Editor {
     public class TextureImportModifier : ScriptableObject
     {
         // 压缩
